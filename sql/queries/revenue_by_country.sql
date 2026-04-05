152d93805408971c24925311cd4e0da7afc9f723
varshitha2308 <varshithareddy9482@gmail.com>
2026-04-05 16:37:43 +0530

    Delete sql/queries directory

--- sql/queries/revenue_by_country.sql
+++
@@ -1,8 +0,0 @@
--- Analyze revenue by country
-
-SELECT
-  country,
-  SUM(total_amount) AS total_revenue
-FROM `conrad-dataops-task.sales_analytics.fact_sales`
-GROUP BY country
-ORDER BY total_revenue DESC;
